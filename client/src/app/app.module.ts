f0cb8ae6b5eeeba9a69d3b783dc2123c532b3aa7
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-05 12:54:04 -0600

    imported the forms and reactive forms modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { DataTableComponent } from './data-table/data-table.component';
 import { CustomCheckboxComponent } from './custom-checkbox/custom-checkbox.component';
-
+import { PaginationComponent } from './pagination/pagination.component';
+import { UserComponent } from './user/user.component';
+import { UserTableComponent } from './user-table/user-table.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchComponent } from './search/search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     DataTableComponent,
-    CustomCheckboxComponent
+    CustomCheckboxComponent,
+    PaginationComponent,
+    UserComponent,
+    UserTableComponent,
+    SearchResultsComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
