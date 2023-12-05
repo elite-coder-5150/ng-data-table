5d0265457b3ece1a819d8444c86bf0fa39a0b66b
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-05 07:48:17 -0600

    defined the user interface

---
+++ client/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    f_name: string;
+    m_name: string;
+    l_name: string;
+    role: string;
+    password: string;
+    email: string;
+    team: string;
+    hire_date: Date;
+}
