eee70f46884b8d152005abc3fc58df29f4cc0531
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-07 11:00:33 -0600

    defined the save and load users methods

--- client/src/app/user-table/user-table.component.ts
+++ client/src/app/user-table/user-table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 @Component({
   selector: 'ng-user-table',
   templateUrl: './user-table.component.html',
@@ -8,6 +10,18 @@ import { User } from '../models/user';
 })
 export class UserTableComponent implements OnInit {
   user: User[] = [];
+  editingUser: boolean = false;
+  currentUser: User = {
+    id: 0,
+      f_name: '',
+      m_name: '',
+      l_name: '',
+      role: '',
+      password: '',
+      email: '',
+      team: '',
+      hire_date: new Date(),
+  };
   constructor(private userService: UserService) {};
 
   ngOnInit(): void {
@@ -16,5 +30,74 @@ export class UserTableComponent implements OnInit {
 
   getUsers(): User[] {
     return this.user;
+  }
+
+  addUser() {
+    const newUser: User = {
+      id: 0,
+      f_name: 'John',
+      m_name: 'Doe',
+      l_name: 'Smith',
+      role: 'User',
+      password: 'password123',
+      email: 'john@example.com',
+      team: 'Team A',
+      hire_date: new Date(),
+    };
+
+    this.userService.addUser(newUser)
+      .pipe(
+        tap(addedUser => {
+          console.log('user added successfully', addedUser);
+        }),
+        catchError(error => {
+          console.error('error adding user', error);
+          return of(null);
+        })
+      ).subscribe();
+    
+  }
+
+  deleteSelected() {
+    const selectedUsers = this.userService
+  }
+  loadUsers() {
+    this.userService.getUsers()
+      .pipe(
+        tap(() => {}), catchError(error => {
+          console.error(error);
+          return of(null)
+        })
+      ).subscribe();
+  }
+  cancelEdit() {}
+
+  saveUser() {
+    if (this.editingUser) {
+      this.userService.updiateUser(this.currentUser)
+        .pipe(
+          tap(() => {
+            this.cancelEdit();
+            this.loadUsers();
+          }),
+          catchError(error => {
+            console.error(error);
+            return of(null);
+          })
+        )
+    } else {
+      this.currentUser.id = this.userService.generateUserId(1, 1000);
+
+      this.userService.addUser(this.currentUser)
+        .pipe(
+          tap(() => {
+            this.cancelEdit();
+            this.loadUsers();
+          }), catchError(error => {
+            console.error(error);
+            return of(null);
+          })
+        ).subscribe();
+    }
   }
 }
