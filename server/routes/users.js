d63a5fa2e1aefba77236ea365a253c915801bfdb
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-11 06:21:44 -0600

    defined the logoutUser route

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,11 +6,14 @@ import {
     getSingleUser, 
     registerUser,
     deleteUser,
+    logInUser,
     logoutUser,
 } from '../controller/index';
+
 router.get('/users', getAllUsers);
 router.get('/users/:id', getSingleUser);
 router.post('/user/register', validateUser, registerUser);
 router.delete('/user/:id', deleteUser);
+router.post('/user/login', logInUser);
 router.post('/user/logout/:id', logoutUser);
 module.exports = router;
